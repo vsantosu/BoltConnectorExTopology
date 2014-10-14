88ee0da8ab11149fcface01a565e23595b671fed
victor Santos Uceta <victor.uceta@gmail.com>
2014-10-14 14:21:09 -0400

    Adding All the Jars

--- src/com/spnotes/storm/bolts/WordCounterBolt.java
+++ src/com/spnotes/storm/bolts/WordCounterBolt.java
@@ -1,21 +1,26 @@
 package com.spnotes.storm.bolts;
 
+import io.netty.channel.ChannelHandlerContext;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.phestorm.boltConnector.BoltConnector;
+import edu.phestorm.boltConnector.BoltConnectorMessage;
+import edu.phestorm.boltConnector.BoltConnectorReader;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Tuple;
 
-public class WordCounterBolt implements IRichBolt{
+public class WordCounterBolt implements IRichBolt {
 
 	Integer id;
 	String name;
 	Map<String, Integer> counters;
 	private OutputCollector collector;
-	
+
 	@Override
 	public void prepare(Map stormConf, TopologyContext context,
 			OutputCollector collector) {
@@ -23,16 +28,16 @@ public class WordCounterBolt implements IRichBolt{
 		this.collector = collector;
 		this.name = context.getThisComponentId();
 		this.id = context.getThisTaskId();
-		
+
 	}
 
 	@Override
 	public void execute(Tuple input) {
 		String str = input.getString(0);
-		if(!counters.containsKey(str)){
+		if (!counters.containsKey(str)) {
 			counters.put(str, 1);
-		}else{
-			Integer c = counters.get(str) +1;
+		} else {
+			Integer c = counters.get(str) + 1;
 			counters.put(str, c);
 		}
 		collector.ack(input);
@@ -40,16 +45,48 @@ public class WordCounterBolt implements IRichBolt{
 
 	@Override
 	public void cleanup() {
-		System.out.println(" -- Word Counter ["+ name + "-"+id +"]");
-		for(Map.Entry<String, Integer> entry:counters.entrySet()){
-			System.out.println(entry.getKey()+" : " + entry.getValue());
+		System.out.println(" -- Word Counter [" + name + "-" + id + "]");
+		for (Map.Entry<String, Integer> entry : counters.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+
+			/* Adding Connector Socket Here */
+			sendViaSocket();
 		}
+	}
+
+	public void sendViaSocket() {
+		
+		BoltConnector bcon = new BoltConnector();
+
+		bcon.setReader(new BoltConnectorReader() {
+
+			@Override
+			public void read(ChannelHandlerContext conCtx,
+					BoltConnectorMessage msg) {
+			}
+
+			@Override
+			public void exceptionCatcher(Throwable cause) {
+				// TODO Auto-generated method stub
+			}
+		});
+
+		bcon.connect("localhost", 8000);
+
+		if (bcon.isConnected())
+		{
+			for (Map.Entry<String, Integer> entry : counters.entrySet())
+				bcon.write("<>"+entry.getKey() + " : " + entry.getValue());
+		}
+		else
+			System.out.println("Client Not Connected!");
+
 	}
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
